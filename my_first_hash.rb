f1581e4c780e9b1d988d90c17284b72a4a4c29eb
Parker Catalano <catalano.work@gmail.com>
2017-10-26 16:59:42 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"brothers" => 2, "cousins" => 6, "parents" => 2}
 
   
 end
@@ -10,7 +10,7 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information: 
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
-
+# 
 end
 
 def retrieval
