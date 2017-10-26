37bdbd386f421f0d773e81757dcf1a870094a45a
Parker Catalano <catalano.work@gmail.com>
2017-10-26 17:08:34 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,6 +20,7 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   #your code here
+  puts shipping_manifest["oil paintings"]
   #remember to return the shipping_manifest hash
-  shipping_manifest["oil paintings"]
+  
 end
